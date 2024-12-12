2091ea20bd43ec5c88b0c6d90cd48654521e3deb
Muhammad Khawar Riaz Abbasi <khawarworkspace@gmail.com>
2024-12-12 17:03:38 +0500

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,13 +4,13 @@ import "./experience.css";
 const ExperiencePortfolio = () => {
   const experienceData = [
     {
-      year: "August 2024 - October 2024",
+      year: "July 2024 - December 2024",
       title: "Frontend Developer",
       description:
-        "During my 3-month internship at EWX, I gained valuable hands-on experience in web development and programming. I worked extensively with HTML, CSS, and Bootstrap, building responsive and visually appealing user interfaces. I honed my JavaScript skills to add interactivity and functionality to web applications. Additionally, I explored C# programming, learning how to create efficient and scalable solutions. This internship allowed me to develop practical skills, work on real-world projects, and enhance my problem-solving abilities, preparing me for future challenges in the field of software development.",
+        "During my 6-month internship at EWX, I gained valuable hands-on experience in web development and programming. I worked extensively with HTML, CSS, and Bootstrap, building responsive and visually appealing user interfaces. I honed my JavaScript skills to add interactivity and functionality to web applications. Additionally, I explored C# programming, learning how to create efficient and scalable solutions. This internship allowed me to develop practical skills, work on real-world projects, and enhance my problem-solving abilities, preparing me for future challenges in the field of software development.",
     },
     {
-      year: "Februry 2024 - July 2024",
+      year: "January 2024 - June 2024",
       title: "Data Entry",
       description:
         "Successfully managed and organized large volumes of data, ensuring a 70% improvement in accuracy and efficiency in data entry processes. Prepared detailed reports on data quality and suggested actionable improvements for streamlining data management. Collaborated with team members to enhance workflows, maintain data integrity, and deliver timely solutions for data-related challenges.",
