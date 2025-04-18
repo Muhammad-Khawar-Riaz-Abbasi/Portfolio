ca94f69c0501310df7f27d45dffcf7ab7f2b0a6e
Muhammad Khawar Riaz Abbasi <khawarworkspace@gmail.com>
2025-04-18 10:24:07 +0500

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,10 @@ const About = () => {
       <div className="content-container">
         <h2 data-aos="fade-up">Who Am I?</h2>
         <p data-aos="fade-up" data-aos-delay="200">
-         I'm Muhammad Khawar Riaz Abbasi, a CS graduate with skills in networking, frontend development, and IT support. I create efficient, user-friendly solutions using HTML, CSS, JavaScript, and MySQL.
+         <p data-aos="fade-up" data-aos-delay="200">
+    I am Muhammad Khawar Riaz Abbasi, a Computer Science graduate specializing in networking, frontend development, and IT systems. Skilled in HTML, CSS, JavaScript, MySQL, and network infrastructure, I have hands-on experience from roles in frontend development and technical support. I am passionate about creating efficient solutions and continuously growing my expertise in technology.
+</p>
+
         </p>
       </div>
     </section>
